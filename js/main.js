ce2194f2fa70e5a6787e70e93d1c04bbe284f852
davidchenbest <davidchen108@gmail.com>
2021-07-16 18:09:09 -0400

    show rate panel

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@ function getVideos() {
   try {
     const videos = document.querySelectorAll("video");
     return videos;
-  } catch (error) {}
+  } catch (error) { }
 }
 
 function stepVideosRate(videos, rate) {
   videos.forEach((v) => {
     v.playbackRate += rate;
   });
+}
+
+function addRatePanel(videos) {
+  const className = 'videoRatePanel'
+  const parent = videos[0].parentNode
+  const exist = parent.querySelector(`.${className}`)
+  if (!exist) {
+    const node = document.createElement("h1");
+    node.innerHTML = '<button>-</button> <button>+</button>'
+    // const textnode = document.createTextNode('<button>video controller</button>');
+    // node.appendChild(textnode);
+    node.style.cssText = 'position:absolute; z-index:10000;'
+    node.className = className
+    parent.append(node)
+  }
+
 }
 
 window.addEventListener("load", (event) => {
@@ -18,8 +34,10 @@ window.addEventListener("load", (event) => {
     const { key } = e;
     if (key === "d" || key === "s") {
       const videos = getVideos();
-      if (videos && videos.length)
+      if (videos && videos.length) {
         stepVideosRate(videos, key === "d" ? fasterRate : slowerRate);
+        addRatePanel(videos)
+      }
     }
   });
 });
